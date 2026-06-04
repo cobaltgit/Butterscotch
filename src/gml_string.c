a2545b4c410f3f06264cb7e309fd4794201df5ac
cobaltgit <cobaltsecuremail312@proton.me>
2026-06-04 13:13:45 +0100

    GMLString: remove malloc bounds checks
    no need, safeMalloc/xmalloc does these already

--- src/gml_string.c
+++ src/gml_string.c
@@ -10,15 +10,10 @@ GMLString* GMLString_create(const char* str) {
     if (!str) return NULL;
 
     size_t len = strlen(str);
+    if (len > INT32_MAX) return NULL; 
 
     GMLString* gmlstr = safeMalloc(sizeof(GMLString));
-    if (!gmlstr) return NULL;
-
     gmlstr->data = safeMalloc(len + 1);
-    if (!gmlstr->data) {
-        free(gmlstr);
-        return NULL;
-    }
 
     memcpy(gmlstr->data, str, len + 1);
 
@@ -43,14 +38,9 @@ GMLString* GMLString_clone(GMLString* src) {
     if (!src) return NULL;
 
     GMLString* dst = safeMalloc(sizeof(GMLString));
-    if (!dst) return NULL;
 
     size_t allocSize = (size_t)src->length + 1;
     dst->data = safeMalloc(allocSize);
-    if (!dst->data) {
-        free(dst);
-        return NULL;
-    }
 
     memcpy(dst->data, src->data, allocSize);
     dst->refCount = 1;
