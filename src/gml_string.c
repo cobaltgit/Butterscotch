85bd2ab47277fbc498e5071ceedd9f5052a77f0c
cobaltgit <cobaltsecuremail312@proton.me>
2026-06-04 13:11:22 +0100

    GMLString_clone: use safeMalloc in place of strdup

--- src/gml_string.c
+++ src/gml_string.c
@@ -45,12 +45,14 @@ GMLString* GMLString_clone(GMLString* src) {
     GMLString* dst = safeMalloc(sizeof(GMLString));
     if (!dst) return NULL;
 
-    dst->data = strdup(src->data);
+    size_t allocSize = (size_t)src->length + 1;
+    dst->data = safeMalloc(allocSize);
     if (!dst->data) {
         free(dst);
         return NULL;
     }
 
+    memcpy(dst->data, src->data, allocSize);
     dst->refCount = 1;
     dst->length = src->length;
     return dst;
